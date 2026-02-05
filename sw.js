737f56fe874cb33fb8e74c4dabeb53aa50717587
kftseng2oo1 <kftseng2oo1@gmail.com>
2026-02-05 10:59:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'iching-pwa-v1';
+const urlsToCache = [
+  './數位易經_優化版_v2_1.html',
+  './manifest.json'
+];
+
+// 安裝 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('快取已開啟');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截網路請求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 快取命中 - 返回快取的資源
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 更新 Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
